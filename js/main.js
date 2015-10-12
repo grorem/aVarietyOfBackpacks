fbfa23ce203207a2933d869d02f4fb8893d6eeae
grorem <gmrorem@gmail.com>
2015-10-12 15:36:55 +0200

    adding a lotta things

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ $(function(){
 
     });
 });
+
+
+$(function() {
+ $('a[href*=#]:not([href=#])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html,body').animate({
+         scrollTop: target.offset().top - 68
+       }, 500);
+       return false;
+     }
+   }
+ });
+});
