3d64e1ff3ac2f46b283af036fb43d153b0130535
grorem <gmrorem@gmail.com>
2015-10-21 12:01:05 +0200

    huge update to newest

--- js/main.js
+++ js/main.js
@@ -1,26 +1,84 @@
 
 
-$(function(){
-    $(".tripBackground").mouseenter(function(){
+$(function() {
+  var i = 1;
 
-      $(".tripBackground").addClass("easeZoom");
+  $(".btncRight").click(function(){
 
+    $(".carryOnSectionMainBack_"+i).fadeOut(400);
+      i++;
+        $(".carryOnSectionMainBack_"+i).delay(.200).fadeIn(600);
+  });
 
-    });
+  $(".btncLeft").click(function(){
+
+    $(".carryOnSectionMainBack_"+i).fadeOut(400);
+      i--;
+        $(".carryOnSectionMainBack_"+i).delay(.200).fadeIn(600);
+  });
+
 });
 
 
 $(function() {
- $('a[href*=#]:not([href=#])').click(function() {
-   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-     var target = $(this.hash);
-     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-     if (target.length) {
-       $('html,body').animate({
-         scrollTop: target.offset().top - 68
-       }, 500);
-       return false;
-     }
-   }
- });
+  var j = 1;
+
+  $(".btnaRight").click(function(){
+
+    $(".backCountry_"+j).fadeOut(0);
+      j++;
+        $(".backCountry_"+j).fadeIn(400);
+  });
+
+  $(".btnaLeft").click(function(){
+
+    $(".backCountry_"+j).fadeOut(0);
+      j--;
+        $(".backCountry_"+j).fadeIn(400);
+  });
+
+});
+
+
+
+
+ $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 100
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+
+$(function(){
+    $(".mobileNavigation a").click(function(){
+
+
+        $('.mobileNavigation').fadeOut();
+
+
+    });
+
+
+
+    $('.gallery').rebox({ selector: 'a' });
+    $('#galleryGranada').rebox({ selector: 'a' });
+    $('#gallerySJ').rebox({ selector: 'a' });
+    $('#galleryMatagalpa').rebox({ selector: 'a' });
+
+
+
+
+
 });
