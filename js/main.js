0e090dcae87c723acac38dab3420d2d4e7cf3981
grorem <gmrorem@gmail.com>
2015-11-28 14:36:28 +0100

    nicafirst and grids

--- js/main.js
+++ js/main.js
@@ -1,4 +1,91 @@
 $(function() {
+  $(".grid1").mouseenter(function(){
+    $(".grid1back").addClass("blurred");
+    $(".grid1Info").fadeIn(250);
+
+  });
+});
+
+$(function() {
+  $(".grid1").mouseleave(function(){
+    $(".grid1back").removeClass("blurred");
+    $(".grid1Info").fadeOut(550);
+  });
+});
+
+$(function() {
+  $(".grid2").mouseenter(function(){
+    $(".grid2back").addClass("blurred");
+    $(".grid2Info").fadeIn(250);
+
+  });
+});
+
+$(function() {
+  $(".grid2").mouseleave(function(){
+    $(".grid2back").removeClass("blurred");
+    $(".grid2Info").fadeOut(550);
+  });
+});
+
+$(function() {
+  $(".grid3").mouseenter(function(){
+    $(".grid3back").addClass("blurred");
+    $(".grid3Info").fadeIn(250);
+
+  });
+});
+
+$(function() {
+  $(".grid3").mouseleave(function(){
+    $(".grid3back").removeClass("blurred");
+    $(".grid3Info").fadeOut(550);
+  });
+});
+
+
+$(function() {
+  $(".grid4").mouseenter(function(){
+    $(".grid4ExtenderUp").slideDown(50);
+    $(".grid4back").addClass("blurred");
+    $(".grid4Info").fadeIn(250);
+
+  });
+});
+
+$(function() {
+  $(".grid4").mouseleave(function(){
+    $(".grid4ExtenderUp").hide(50);
+    $(".grid4back").removeClass("blurred");
+    $(".grid4Info").fadeOut(550);
+  });
+});
+
+$(function() {
+  $(".grid5").mouseenter(function(){
+    $(".grid5ExtenderUp").slideDown(50);
+    $(".grid5back").addClass("blurred");
+    $(".grid5Info").fadeIn(250);
+
+  });
+});
+
+$(function() {
+  $(".grid5").mouseleave(function(){
+    $(".grid5ExtenderUp").hide(50);
+    $(".grid5back").removeClass("blurred");
+    $(".grid5Info").fadeOut(550);
+  });
+});
+
+
+$(function() {
+  $(".afSnow").mouseout(function(){
+    $(".extenderUp").hide(50);
+  });
+});
+
+$(function() {
   $(".mainPic").mouseenter(function(){
     $(".mainPiccta").fadeIn(50);
   });
